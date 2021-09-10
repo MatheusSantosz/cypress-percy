ce117ca5083f93a8a287a702b55726ae42a82063
MatheusSantosz <matheu.santos.ciencia@gmail.com>
2021-09-10 08:08:25 -0300

    c

--- cypress/integration/ticketbox.spec.js
+++ cypress/integration/ticketbox.spec.js
@@ -2,7 +2,9 @@
 describe('TicketBOx', () => {
     beforeEach(() => cy.visit('https://ticket-box.s3.eu-central-1.amazonaws.com/index.html'));
     it('Check for the Initial state', () => {
-        cy.percySnapshot() 
+        cy.wait(5000)
+        cy.percySnapshot('form') 
+        cy.screenshot('form')
      });
  
 
